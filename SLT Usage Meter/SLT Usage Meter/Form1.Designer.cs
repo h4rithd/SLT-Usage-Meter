8e390b59cf97bb0fdd205c1527ba0eaa4edad662
h4rithd <root@h4rithd.h4rithd>
2021-08-26 22:29:25 +0530

    Add files

---
+++ SLT Usage Meter/SLT Usage Meter/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace SLT_Usage_Meter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtVCode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.GroupBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.Result.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(49)))), ((int)(((byte)(63)))));
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUser.Font = new System.Drawing.Font("Candara", 9.75F);
+            this.txtUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.txtUser.Location = new System.Drawing.Point(117, 25);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(158, 16);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Enter += new System.EventHandler(this.txtUser_Enter);
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(49)))), ((int)(((byte)(63)))));
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPass.Font = new System.Drawing.Font("Candara", 9.75F);
+            this.txtPass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.txtPass.Location = new System.Drawing.Point(118, 59);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(151, 16);
+            this.txtPass.TabIndex = 2;
+            this.txtPass.Enter += new System.EventHandler(this.txtPass_Enter);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(172, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Loging";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(143, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(207)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "SLT UM by #Shap_Manasick";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(108)))), ((int)(((byte)(179)))));
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(461, 20);
+            this.panel1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.label2.Location = new System.Drawing.Point(13, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Portal Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara", 9.75F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.label3.Location = new System.Drawing.Point(14, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Portal Password";
+            // 
+            // txtVCode
+            // 
+            this.txtVCode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(49)))), ((int)(((byte)(63)))));
+            this.txtVCode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtVCode.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVCode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.txtVCode.Location = new System.Drawing.Point(126, 142);
+            this.txtVCode.Name = "txtVCode";
+            this.txtVCode.Size = new System.Drawing.Size(151, 20);
+            this.txtVCode.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Candara", 9.75F);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.label4.Location = new System.Drawing.Point(14, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Verify Code\t";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(207)))), ((int)(((byte)(224)))));
+            this.label5.Location = new System.Drawing.Point(289, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "X";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtUser);
+            this.groupBox1.Controls.Add(this.txtVCode);
+            this.groupBox1.Controls.Add(this.txtPass);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.groupBox1.Location = new System.Drawing.Point(8, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 207);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loging";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.groupBox2.Location = new System.Drawing.Point(16, 357);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(269, 101);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Data Usage";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.label6.Location = new System.Drawing.Point(116, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "______________________";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.label7.Location = new System.Drawing.Point(114, 61);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(161, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "______________________";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.label8.Location = new System.Drawing.Point(121, 148);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(161, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "______________________";
+            // 
+            // Result
+            // 
+            this.Result.Controls.Add(this.button2);
+            this.Result.Controls.Add(this.label10);
+            this.Result.Controls.Add(this.lblTotal);
+            this.Result.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.Result.Location = new System.Drawing.Point(10, 247);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(292, 132);
+            this.Result.TabIndex = 14;
+            this.Result.TabStop = false;
+            this.Result.Text = "Result";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Candara", 9.75F);
+            this.lblTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.lblTotal.Location = new System.Drawing.Point(22, 72);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(43, 15);
+            this.lblTotal.TabIndex = 16;
+            this.lblTotal.Text = "Total : \t";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Candara", 9.75F);
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(139)))), ((int)(((byte)(195)))));
+            this.label10.Location = new System.Drawing.Point(22, 98);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Peek :";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(337, 55);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser1.TabIndex = 15;
+            this.webBrowser1.Url = new System.Uri("https://www.internetvas.slt.lk/SLTVasPortal-war/application/usage.nable", System.UriKind.Absolute);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(171, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Get Result";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(49)))), ((int)(((byte)(63)))));
+            this.ClientSize = new System.Drawing.Size(312, 392);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Result.ResumeLayout(false);
+            this.Result.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtVCode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox Result;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SLT Usage Meter/SLT Usage Meter/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mshtml;
+using System.Threading;
+using System;
+
+
+
+namespace SLT_Usage_Meter
+{
+    public partial class Form1 : Form
+    {
+             
+        public Form1()
+        {
+            InitializeComponent();
+            Clipboard.Clear();
+            this.Size = new Size(310, 10);
+            
+            webBrowser1.Navigate("https://www.internetvas.slt.lk/SLTVasPortal-war/application/login.nable");
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser_DocumentCompleted);
+            
+        }
+
+        public void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.Size = new Size(312, 392);
+           
+            CopyClip();
+            getClip();
+
+
+        }
+
+
+        private void getClip() {
+
+            Image cImage = Clipboard.GetImage();
+            pictureBox1.Image = (Image)cImage;
+            
+        }
+
+        public void CopyClip() {
+
+            try
+            {
+                string image_name = "temp.bmp";
+                IHTMLDocument2 document = (IHTMLDocument2)webBrowser1.Document.DomDocument;
+                IHTMLControlRange imgRange = (IHTMLControlRange)((HTMLBody)document.body).createControlRange();
+                Clipboard.Clear();
+                imgRange.add(document.all.item("loginfrm:j_idt11"));
+                imgRange.execCommand("Copy");
+                using (Bitmap bmp = (Bitmap)Clipboard.GetDataObject().GetData(DataFormats.Bitmap))
+                {
+                    bmp.Save(image_name);
+                }
+               
+            }
+            catch (Exception x) {
+               
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            String userName = txtUser.Text;
+            String passWord = txtPass.Text;
+            String vCode = txtVCode.Text;
+
+            webBrowser1.Document.GetElementById("loginfrm:chngusername").SetAttribute("Value", userName);
+            webBrowser1.Document.GetElementById("loginfrm:chngpassword").SetAttribute("Value", passWord);
+            webBrowser1.Document.GetElementById("loginfrm:chngcaptchs").SetAttribute("Value", vCode);
+
+            webBrowser1.Document.GetElementById("loginfrm:logbtn").InvokeMember("Click");
+
+            MessageBox.Show("Click Get Result Button ! :)");
+            geReseut();
+            final();
+
+        }
+
+
+        public void geReseut() {
+
+           
+            webBrowser1.Navigate("https://www.internetvas.slt.lk/SLTVasPortal-war/application/usage.nable");
+                       
+        }
+
+        public void final() {
+
+            HtmlElementCollection theElementCollection = default(HtmlElementCollection);
+            theElementCollection = webBrowser1.Document.GetElementsByTagName("div");
+
+            foreach (HtmlElement curElement in theElementCollection)
+            {
+                if (curElement.GetAttribute("className").ToString() == "i_detail_peak")
+                {
+                    lblTotal.Text = curElement.GetAttribute("InnerText");
+
+                }
+
+            }
+            foreach (HtmlElement curElement in theElementCollection)
+            {
+                if (curElement.GetAttribute("className").ToString() == "i_detail")
+                {
+                    label10.Text = curElement.GetAttribute("InnerText");
+
+                }
+
+            }
+
+        }
+
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                      
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void txtPass_Enter(object sender, EventArgs e)
+        {
+            
+                    }
+
+        private void txtUser_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            HtmlElementCollection theElementCollection = default(HtmlElementCollection);
+            theElementCollection = webBrowser1.Document.GetElementsByTagName("div");
+
+            foreach (HtmlElement curElement in theElementCollection)
+            {
+                if (curElement.GetAttribute("className").ToString() == "i_detail_peak")
+                {
+                    //ssageBox.Show(curElement.GetAttribute("InnerText"));
+                    lblTotal.Text = curElement.GetAttribute("InnerText");
+                    // Do something you want
+
+                }
+
+            }
+            foreach (HtmlElement curElement in theElementCollection)
+            {
+                if (curElement.GetAttribute("className").ToString() == "i_detail")
+                {
+                    //ssageBox.Show(curElement.GetAttribute("InnerText"));
+                    label10.Text = curElement.GetAttribute("InnerText");
+                    // Do something you want
+
+                }
+
+            }
+        }
+    }
+
+}
